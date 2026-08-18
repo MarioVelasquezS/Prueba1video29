a49489731a053b816213414f4aec92dd8e83230a
MarioVelasquezS <mavs17all@gmail.com>
2026-08-17 18:01:06 -0600

    codigo basico de jsos

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const PORT = 3000;
 //req es la solicitud
 //res es para responder 
 //el parametro de ruta lo defino aca (/:en esta parte le doy un nombre )
-app.get('/:miparametro', (req, res)=>{
-   const miparametro= req.params.miparametro;
-   res.send('ola como estas desde express....'+ miparametro);
+// app.get('/:miparametro', (req, res)=>{
+//    const miparametro= req.params.miparametro;
+//    res.send('ola como estas desde express....'+ miparametro);
 
-});
+// });
 
 //segunda forma de obtener parametros:
 //en la constanta el valor siempre va a venir del req.
@@ -22,6 +22,14 @@ app.get('/queryString',(req, res)=>{
     const valor =req.query.valor;
     res.send(`El valor es: ${valor}`)    //(`` esas comillas asi significa interpolar)
 });
+
+//este codigo es un objeto JSON solo me llevo la ruta y la coloco 
+// en el navegador y lo pruebo a ver si funciona 
+app.get('/ejerciciojson',(req, res)=>{
+    res.json({status:200,message:"Success",data:"Hello word desde el express"});
+});
+
+
 
 app.listen(PORT,()=>{
     console.log(`El servidor esta escuchando en http://localhost:${PORT}`);
