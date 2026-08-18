9e613c3d8cd5db7813b8844b543986490fa10494
MarioVelasquezS <mavs17all@gmail.com>
2026-08-18 17:57:18 -0600

    video 5 1 hora 11 minutos

---
+++ MiprimerCRUC/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+//vamos a crear un objeto JSON
+//lo que hago es reservar un espacio en memoria para que se pueda ejecutar. 
+const users = [{nombre: "mario velasquez", edad:30, correo:"mavs17al@hotmail.com"},{nombre: "liliam pamela romero", edad:90, correo:"lilaimprome@hotmail.com"}]
+
+//vamoa a reac, que es el obtener datos
+//res= la respuesta que vamos a enviar
+//req=todo lo que el usuario envia en la peticion que es no mas codigo
+app.get("/user",(req, res)=>{
+
+    res.status(200).json({status:200,message:"success",data: users});
+
+});
+
+//para poder filtrar los datos del JSON anterior 
+//en este caso lo haremos por correo
+
+app.get("/user/:correo",(req, res)=>{
+    const correo = req.params.correo;
+    let isActive= false;
+   users.forEach(user=> {
+     if(user.correo === correo){
+        isActive= true;
+        return res.status(200).json({status:200, message:"Success", data:user});
+
+     }
+   });
+
+   if(!isActive){
+    return res.status(400).json({status:404, message:"registro no encontrado", data:null})
+    }
+});
+
+app.post("/user",(req, res)=>{
+   const user = req.body;
+
+   users.push(user);
+   res.status(200).json({status:200, message:"Success", data:user});
+});
+
+
+
+//vamos a obtener los datos: 
+//para que el servidor escuche en que puerto estamos trabajando
+app.listen(port, ()=>{
+    console.log(`el servidor de exprres esta escuchando en http://localhost:300:${port}`);
+    });
